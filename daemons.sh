3ec1579b7efb2895354fb331b4a01410a3ed4d7e
Pandu Rao <pandu@fanmarker.com>
2015-05-03 11:34:54 -0700

    Changed daemons script to use tmux and layout

--- daemons.sh
+++ daemons.sh
@@ -1,20 +1,63 @@
 #!/bin/bash
 
-echo "Launching storm daemons in their own screens"
+SESSION=$USER
 
-echo "Launching zookeeper screen ..."
-screen -S 'zookeeper'  -d -m && screen -r 'zookeeper'  -X stuff $'zkServer.sh start\n'
+# Test if the session has windows
+function is_closed(){
+    n=$(tmux ls 2> /dev/null | grep "^$sess" | wc -l)
+    [[ $n -eq 0 ]]
+}
 
-echo "Launching nimbus screen ..."
-screen -S 'nimbus'     -d -m && screen -r 'nimbus'     -X stuff $'storm nimbus\n'
+function split_window() {
+    byobu-tmux split-window -v;
+    byobu-tmux select-layout tiled
+}
 
-echo "Launching supervisor screen ..."
-screen -S 'supervisor' -d -m && screen -r 'supervisor' -X stuff $'storm supervisor\n'
+function make_pane() {
+    id=$1;
+    cmd=$2;
+    byobu-tmux select-pane -t "$id"
+    byobu-tmux send-keys "$cmd" C-m
+}
 
-echo "Launching drpc screen ..."
-screen -S 'drpc'       -d -m && screen -r 'drpc'       -X stuff $'storm drpc\n'
+function attach_session() {
+    id=$1;
+    byobu-tmux -2 attach-session -t $SESSION:"$id"
+}
 
-echo "Launching ui screen ..."
-screen -S 'ui'         -d -m && screen -r 'ui'         -X stuff $'storm ui\n'
 
-screen -ls
+# Attach to existing session or create a new one
+if ! is_closed; then
+    attach_session 0
+else
+    # -2: forces 256 colors,
+    byobu-tmux -2 new-session -d -s $SESSION
+
+    # Start window for daemons
+    byobu-tmux rename-window -t $SESSION:0 'daemons'
+
+    split_window
+    make_pane 0 "zkServer.sh start-foreground";
+    make_pane 1 "storm nimbus";
+
+    split_window
+    make_pane 2 "storm supervisor";
+
+    split_window
+    make_pane 3 "storm drpc";
+
+    split_window
+    make_pane 4 "storm ui";
+
+    # Set default window
+    byobu-tmux select-window -t $SESSION:0
+
+    # Attach to new session, flip between windows using m-l, m-r
+    attach_session 0
+fi
+
+# The session is now either closed or detatched
+if is_closed; then
+    # Perform cleanup here as needed
+    :
+fi
