cdfd615b427f92cf86b0757f1ca4eb48e041bb5d
Pandu Rao <pandu@fanmarker.com>
2015-05-18 11:41:46 -0700

    Added guard to check for nimbus node

--- dds.sh
+++ dds.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # get the address of the dockerized drpc server
+# note: nimbus and drpc servers are typically combined
+# so we lookup id of nimbus node
 
 # get the container id
 nimbus_id=$(docker ps | grep nimbus | awk '{print $1}')
+
+if [ -z "$nimbus_id" ]; then
+    echo "Failed to find nimbus node. Is storm-docker running?"
+    exit -1;
+fi
 
 # inspect the container for its ip address
 docker inspect $nimbus_id | grep -i ipaddress | awk '{print $2}' | sed -e 's/[",]//g'
